2da1a83a3b961f4a8d5a2500f9e5f34753f2ede2
Vinicius Santos da Silva <38990775+iamviniciuss@users.noreply.github.com>
2024-04-28 20:31:17 -0300

    set port equals to 80

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
-const PORT = 5000;
+const PORT = 80;
+
+setInterval(() => {
+    console.log("rodando...")
+}, 5000)
 
 app.get("/", (req, res) => {
     console.log("hellooow!");

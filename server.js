f638fd8648c5bbcf071e9f5c62e72b409d6b2220
Vinicius Santos da Silva <38990775+iamviniciuss@users.noreply.github.com>
2024-04-28 20:13:18 -0300

    update task definition

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const app = express();
 const PORT = 5000;
 
 app.get("/", (req, res) => {
+    console.log("hellooow!");
     res.send("Hello world!")
 });
 
 app.listen(PORT, () => {
-  console.log("Server running 3");
+  console.log("Server running on port: "+PORT);
 });
